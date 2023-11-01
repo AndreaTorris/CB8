4069d6b7c9e37ee7c122210d3d4e3510c6653fea
AndreaTorris <148492124+AndreaTorris@users.noreply.github.com>
2023-11-01 10:37:01 +0100

    Add files via upload

---
+++ 01-11-2023/script.js
@@ -0,0 +1,14 @@
+const AnimalArray = [
+  "Elefante",
+  "alce",
+  "corvo",
+  "gallina",
+  "macaco",
+  "leone",
+  "tigre",
+  "ippopotamo",
+];
+
+for (let i = 0; i < AnimalArray.length; i++) {
+  console.log(i + 1 + ": " + AnimalArray[i]);
+}
