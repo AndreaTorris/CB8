1670f2542305b14b3333d3fa6c179fb6465bfacb
AndreaTorris <148492124+AndreaTorris@users.noreply.github.com>
2023-12-14 17:49:31 +0100

    Add files via upload

---
+++ Esercitazione_react-second-app/src/components/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import FooterList from "../footerList/FooterList";
+("../footerList/FooterList");
+
+const Footer = () => {
+  return (
+    <footer className="Footer">
+      <p>2023 My Website</p>
+      <FooterList />
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ Esercitazione_react-second-app/src/components/footerList/FooterList.jsx
@@ -0,0 +1,19 @@
+import "./index.css";
+
+const FooterList = () => {
+  return (
+    <ul class="footer-list">
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Contact</a>
+      </li>
+    </ul>
+  );
+};
+
+export default FooterList;
